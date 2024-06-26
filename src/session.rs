7491057d0925fa763244c98d5fa40b04b71ce6ad
Pedro Serrano Faria <pedroserrano2@gmail.com>
2024-06-25 21:20:22 -0300

    added resend request

--- src/session.rs
+++ src/session.rs
@@ -1,13 +1,15 @@
 use quickfix::{
     ApplicationCallback, Message, SessionSettings, SessionId, QuickFixError,
     FileMessageStoreFactory, Application, SocketInitiator, ConnectionHandler,
-    MsgToAppError, MsgFromAdminError, MsgFromAppError, LogFactory,
+    MsgToAppError, MsgFromAdminError, MsgFromAppError, LogFactory, FieldMap,
+    send_to_target,
 };
 use crate::config::AppConfig;
 use crate::handler::MessageHandler;
 use crate::custom_logger::CustomLogger;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use std::io::Read;
-use std::sync::Arc;
 use std::thread;
 
 pub fn start_sessions(config: AppConfig, handler: Arc<MessageHandler>) -> Result<(), QuickFixError> {
@@ -35,10 +37,12 @@ fn start_single_session(settings_file: String, handler: Arc<MessageHandler>) -> 
     let store_factory = FileMessageStoreFactory::try_new(&settings)?;
     let logger = CustomLogger::new("logs/raw.log", "logs/human_readable.log")?;
     let log_factory = LogFactory::try_new(&logger)?;
-    let app_instance = DropCopyApplication { handler };
+    let app_instance = DropCopyApplication {
+        handler,
+        message_store: Arc::new(Mutex::new(HashMap::new())),
+    };
     let app = Application::try_new(&app_instance)?;
 
-    // Inicialize o SocketInitiator
     let mut initiator = SocketInitiator::try_new(&settings, &app, &store_factory, &log_factory)?;
     initiator.start()?;
 
@@ -58,6 +62,7 @@ fn start_single_session(settings_file: String, handler: Arc<MessageHandler>) -> 
 
 struct DropCopyApplication {
     handler: Arc<MessageHandler>,
+    message_store: Arc<Mutex<HashMap<u64, Message>>>,
 }
 
 impl ApplicationCallback for DropCopyApplication {
@@ -82,15 +87,58 @@ impl ApplicationCallback for DropCopyApplication {
         Ok(())
     }
 
-    fn on_msg_from_admin(&self, msg: &Message, _session_id: &SessionId) -> Result<(), MsgFromAdminError> {
+    fn on_msg_from_admin(&self, msg: &Message, session_id: &SessionId) -> Result<(), MsgFromAdminError> {
         println!("Message from admin: {:?}", msg);
+        self.store_message(msg);
         self.handler.on_message(msg);
+        self.handle_message(msg, session_id);
         Ok(())
     }
 
-    fn on_msg_from_app(&self, msg: &Message, _session_id: &SessionId) -> Result<(), MsgFromAppError> {
+    fn on_msg_from_app(&self, msg: &Message, session_id: &SessionId) -> Result<(), MsgFromAppError> {
         println!("Message from app: {:?}", msg);
+        self.store_message(msg);
         self.handler.on_message(msg);
+        self.handle_message(msg, session_id);
         Ok(())
+    }
+}
+
+impl DropCopyApplication {
+    fn store_message(&self, msg: &Message) {
+        let seq_no: u64 = msg.get_field(34).unwrap().parse().unwrap();
+        let mut store = self.message_store.lock().unwrap();
+        store.insert(seq_no, msg.clone());
+    }
+
+    fn handle_message(&self, msg: &Message, session_id: &SessionId) {
+        match msg.get_field(35).as_deref() {
+            Some("2") => {
+                self.handle_resend_request(msg, session_id);
+            }
+            _ => {
+                println!("Received message: {:?}", msg);
+            }
+        }
+    }
+
+    fn handle_resend_request(&self, msg: &Message, session_id: &SessionId) {
+        let begin_seq_no: u64 = msg.get_field(7).unwrap().parse().unwrap();
+        let end_seq_no: u64 = msg.get_field(16).unwrap().parse().unwrap();
+
+        println!("Received Resend Request from {} to {}", begin_seq_no, end_seq_no);
+
+        for seq_no in begin_seq_no..=end_seq_no {
+            if let Some(stored_message) = self.get_message(seq_no) {
+                let mut resend_message = stored_message.clone();
+                resend_message.set_field(43, "Y").unwrap();
+                send_to_target(resend_message, session_id).unwrap();
+            }
+        }
+    }
+
+    fn get_message(&self, seq_no: u64) -> Option<Message> {
+        let store = self.message_store.lock().unwrap();
+        store.get(&seq_no).cloned()
     }
 }
